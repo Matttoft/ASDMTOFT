7365f4f534222a9e22bbda9577cdc8bc1c0cc19f
Matt Toft <matttoft@gmail.com>
2012-02-16 16:10:53 -0500

    more w3

--- AxisApp/AsdApp/app/_attachments/couchview.js
+++ AxisApp/AsdApp/app/_attachments/couchview.js
@@ -1,13 +1,53 @@
 $(document).ready(function(){
+	$('#json').bind('click', function(){
+		$('#browse').empty();
 	$.ajax({
 		"url":"_view/techteam",
 		"dataType":"json",
 		"success": function(data){
 			$.each(data.rows, function(index,tech){
+				var fname=tech.value.fname;
+				var lname=tech.value.lname;
+				var email=tech.value.email;
+				var tel=tech.value.email;
+				var sex=tech.value.sex;
+				var min=tech.value.ministry;
+				var partner=tech.value.memtype;
+				var newDiv = document.createElement("div");
+
+				var newh3 = document.createElement("h3");
 				
+				var setdiv = newDiv.setAttribute("data-role", "fieldcontain");
+				$('<h3 />', {
+					text : fname +" "+ lname
+				}).appendTo(newDiv);
+				var setdiv = newDiv.setAttribute("data-role", "fieldcontain");
+
+				$('<p />', {
+					text : 'Email: ' + email
+				}).appendTo(newDiv);
+
+				$('<p />', {
+					text : 'Phone: ' + tel
+				}).appendTo(newDiv);
+
+				$('<p />', {
+					text : 'Sex: ' + sex
+				}).appendTo(newDiv);
+				
+				$('<p />', {
+					text : 'Axis Partner?: ' + partner
+				}).appendTo(newDiv);
+
+				$('<p />', {
+					text : 'Ministry: ' + min
+				}).appendTo(newDiv);
+						
 			});
 		}
 		
 	});
 	
+});
+	return false;
 });
--- AxisApp/AsdApp/app/views/techteam/map.js
+++ AxisApp/AsdApp/app/views/techteam/map.js
@@ -1,5 +1,13 @@
 function(doc) {
   if (doc._id.substr(0,5)==='tech:') {
-    emit(doc._id);
+    emit(doc._id, {
+    	"fname":doc.fname,
+    	"lname":doc.lname,
+    	"email":dpc.email,
+    	"tel":doc.tel,
+    	"sexval":doc.sexval,
+    	"ministry":doc.ministry,
+    	"memtype":doc.memtype
+    });
   }
 };
